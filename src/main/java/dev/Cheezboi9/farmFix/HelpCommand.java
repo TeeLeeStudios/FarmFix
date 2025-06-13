39731396d2d5e94dce6dc8f0d26e00c7562dae27
Thomas <teeleestudios@gmail.com>
2025-06-13 04:37:50 -0700

    Adjusting readability of code, added default permissions, cleaned up help command

--- src/main/java/dev/Cheezboi9/farmFix/HelpCommand.java
+++ src/main/java/dev/Cheezboi9/farmFix/HelpCommand.java
@@ -8,38 +8,24 @@ import org.jetbrains.annotations.NotNull;
 
 public class HelpCommand implements CommandExecutor {
 
-  private final String[] HELP_COMMANDS = {"help", "-h"};
+  private static final String[] HELP_COMMANDS = {"help", "-h"};
+  private static final MiniMessage MM = MiniMessage.miniMessage();
 
-  /* SUB_COMMANDS is a paired array [command usage, permission]
-     and uses a 1 dimensional array for less overhead.
-     I considered using a map but realistically, I'm not going to be adding more commands
-  */
+  // I'm using an array rather than a list for the purposes of this test, but I would use a list of records here
+  /** SUB_COMMANDS is a paired array [command usage, permission]
+    * and uses a 1 dimensional array for less overhead.
+    * I considered using a map but realistically, I'm not going to be adding more commands
+    */
   private final String[] SUB_COMMANDS = {
-      "/ff trample [opt: player] [opt: forced] - Toggles ability to trample crops", FarmPerms.TRAMPLE,
+      "/trample <opt: player> [opt: forced <true,1,false,0> ] - Toggles ability to trample crops for other players, and optionally force it", FarmPerms.MOD,
+      "/trample - Toggles ability to trample crops", FarmPerms.TRAMPLE,
   };
 
-  /**
-   * Handles commands '/ff help' and '/ff -h' to display help context menu
-   */
   @Override
   public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command,
                            @NotNull String s, @NotNull String[] args) {
-    // Case-insensitive command argument check
-    if (args.length == 0 || isHelpCommand(args[0])) {
-      sendHelpContext(commandSender);
-      return true;
-    }
-    return false;
-  }
-
-  // Extracted the following functions for readability
-  private boolean isHelpCommand(String arg) {
-    for (String command : HELP_COMMANDS) {
-      if (arg.equalsIgnoreCase(command)) {
-        return true;
-      }
-    }
-    return false;
+    sendHelpContext(commandSender);
+    return true;
   }
 
   private void sendHelpContext(CommandSender sender) {
@@ -47,13 +33,15 @@ public class HelpCommand implements CommandExecutor {
     String formatPrefix = "<b><green>[FarmFix] </green></b><yellow>";
     String formatSuffix = "</yellow>";
     String headerText = formatPrefix + "-== Available Help Options ==-" + formatSuffix;
-    MiniMessage mm = MiniMessage.miniMessage();
-    sender.sendMessage(mm.deserialize(headerText));
+    sender.sendMessage(MM.deserialize(headerText));
 
-    // First, iterate by 2 to check if they have the correct permission, and send the command usage (i-1) if they do
-    for (int i = 1; i < SUB_COMMANDS.length; i += 2) {
-      if (sender.hasPermission(SUB_COMMANDS[i]) || FarmPerms.isMod(sender)) {
-        sender.sendMessage(mm.deserialize(formatPrefix + SUB_COMMANDS[i - 1] + formatSuffix));
+    // Only sends usage of commands the user has access to
+    for (int i = 0; i < SUB_COMMANDS.length; i += 2) {
+      String usage = SUB_COMMANDS[i];
+      String permission = SUB_COMMANDS[i + 1];
+
+      if (sender.hasPermission(permission) || FarmPerms.isMod(sender)) {
+        sender.sendMessage(MM.deserialize(formatPrefix + usage + formatSuffix));
       }
     }
   }
