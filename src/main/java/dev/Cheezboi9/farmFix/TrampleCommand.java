9a7f5e102f55b3f055fb3763424b73e62b08b610
Thomas <teeleestudios@gmail.com>
2025-06-14 18:20:34 -0700

    Fixed incorrect handling of /trample input with all its additional arguments

--- src/main/java/dev/Cheezboi9/farmFix/TrampleCommand.java
+++ src/main/java/dev/Cheezboi9/farmFix/TrampleCommand.java
@@ -16,56 +16,59 @@ public class TrampleCommand implements CommandExecutor {
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
                            @NotNull String label, @NotNull String[] args) {
     boolean isPlayer = sender instanceof Player;
-    if (!isPlayer && args.length == 0) { // Executing this from console without specifying another player
-      sender.sendMessage("You must be a player to toggle trample this way");
-      return false;
-    }
 
-    // I'm assuming online mode servers, but I can change this to support offline servers
-    OfflinePlayer offlinePlayer;
-    if (isPlayer) {
+    if (args.length == 0) {
+      // Early return for console trying to use trample incorrectly
+      if (!isPlayer) {
+        sender.sendMessage("You must be a player to toggle trample this way");
+        return true;
+      }
+      // Early return if no perms to toggle trample
       if (!(sender.hasPermission(FarmPerms.TRAMPLE))) {
         sender.sendMessage("You do not have permission to toggle your trample");
         return true;
       }
-      offlinePlayer = (Player) sender;
-    } else { // Executing this from Console
-      offlinePlayer = Bukkit.getOfflinePlayer(args[1]);
-      if (!offlinePlayer.hasPlayedBefore()) {
-        sender.sendMessage("The specified player doesn't exist. Please check for spelling");
-        return true;
-      }
-    }
-    UUID uuid = offlinePlayer.getUniqueId();
-
-    if (args.length == 0) {
-      if (TrampleManager.isForced(uuid) && !FarmPerms.isMod(sender)) {
+      Player player = (Player) sender;
+      // Early return if we don't have perms or forced toggle
+      if (TrampleManager.isForced(player.getUniqueId()) && !FarmPerms.isMod(player)) {
         sender.sendMessage("Unable to toggle as your trample is set by a mod");
         return true;
       }
-      boolean newState = TrampleManager.toggleTrample(uuid, false);
+
+      boolean newState = TrampleManager.toggleTrample(player.getUniqueId(), false);
       sender.sendMessage("Trample is now: " + newState);
       return true;
     }
 
-    // Handles /trample <player> [opt: forced]
+    // Only allows mods to use /trample <player> [opt: forced]
     if (!FarmPerms.isMod(sender)) {
       sender.sendMessage("You do not have permissions to toggle other player's trample");
       return true;
     }
 
-    boolean newState;
-    if (args.length == 1) { // Handling /trample <player>
-      newState = TrampleManager.toggleTrample(uuid, false);
-    } else { // Finally handling the [forced] tag
-      boolean forced = (args[1].equalsIgnoreCase("true") || args[1].equals("1"));
-      newState = TrampleManager.toggleTrample(uuid, forced);
+    // Handling /trample <player>
+    OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(args[0].trim());
+    if (!offlinePlayer.hasPlayedBefore()) {
+      sender.sendMessage("Unable to find player");
+      return true;
     }
+
+    boolean newState, toForce;
+    UUID uuid = offlinePlayer.getUniqueId();
+
+    // If the [opt: forced] flag has been specified
+    if (args.length >= 2) {
+      toForce = args[1].equalsIgnoreCase("true") || args[1].equals("1");
+    }
+    else {
+      toForce = false;
+    }
+
+    newState = TrampleManager.toggleTrample(uuid, toForce);
 
     // Informing the user of changes
     sender.sendMessage(args[0] + "'s trample is now: " + newState +
         " and mod forced is: " + TrampleManager.isForced(uuid));
     return true;
-
   }
 }
