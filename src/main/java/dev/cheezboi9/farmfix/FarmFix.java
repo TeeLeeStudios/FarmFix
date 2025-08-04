e8d5cbebad114c1c7b634a51606f832b64904a10
Thomas <teeleestudios@gmail.com>
2025-08-04 02:09:24 -0700

    Added implementation for Database and new Mob Trample command

--- src/main/java/dev/cheezboi9/farmfix/FarmFix.java
+++ src/main/java/dev/cheezboi9/farmfix/FarmFix.java
@@ -2,8 +2,10 @@ package dev.cheezboi9.farmfix;
 
 import dev.cheezboi9.farmfix.commands.ForcedCommand;
 import dev.cheezboi9.farmfix.commands.HelpCommand;
+import dev.cheezboi9.farmfix.commands.MobTrampleCommand;
 import dev.cheezboi9.farmfix.commands.TrampleCommand;
 import dev.cheezboi9.farmfix.eventhandlers.*;
+import dev.cheezboi9.farmfix.managers.DatabaseManager;
 import dev.cheezboi9.farmfix.managers.TrampleManager;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,10 +14,11 @@ import java.util.Objects;
 
 public final class FarmFix extends JavaPlugin {
   private static TrampleManager trampleManager;
+  private static DatabaseManager databaseManager;
   @Override
   public void onEnable() {
     trampleManager = new TrampleManager();
-    trampleManager.loadConfig();
+    databaseManager = new DatabaseManager();
     registerCommands();
     registerEventListeners();
     getLogger().info("Loaded Plugin");
@@ -23,15 +26,16 @@ public final class FarmFix extends JavaPlugin {
 
   @Override
   public void onDisable() {
-    getLogger().info("Unloaded Plugin");
     // Save our config so it's not lost when we disable our plugin
-    trampleManager.saveConfig();
+    databaseManager.close();
+    getLogger().info("Unloaded Plugin");
   }
 
   public void registerCommands() {
     Objects.requireNonNull(this.getCommand("ff")).setExecutor(new HelpCommand());
     Objects.requireNonNull(this.getCommand("trample")).setExecutor(new TrampleCommand());
     Objects.requireNonNull(this.getCommand("forced")).setExecutor(new ForcedCommand());
+    Objects.requireNonNull(this.getCommand("mobtrample")).setExecutor(new MobTrampleCommand());
   }
 
   public void registerEventListeners() {
@@ -45,5 +49,9 @@ public final class FarmFix extends JavaPlugin {
 
   public static TrampleManager getTrampleManager(){
     return trampleManager;
+  }
+
+  public static DatabaseManager getDatabaseManager(){
+    return databaseManager;
   }
 }
