8fe9cbb19c27c300fd1533a1ee2a745f3818128c
Thomas <teeleestudios@gmail.com>
2025-08-04 02:06:59 -0700

    Added Database support and removed original config file for handling saving of trample states

---
+++ src/main/java/dev/cheezboi9/farmfix/managers/DatabaseManager.java
@@ -0,0 +1,74 @@
+package dev.cheezboi9.farmfix.managers;
+
+import com.mongodb.client.*;
+import com.mongodb.client.model.UpdateOptions;
+import org.bson.Document;
+
+import java.util.UUID;
+
+public class DatabaseManager implements AutoCloseable{
+  // MongoDB connection string
+  private final static String URI = "mongodb+srv://<Ask Cheezboi9>@cheezenvironment.2apl6re.mongodb.net/?retryWrites=true&w=majority&appName=CheezEnvironment";
+  private final MongoClient mongoClient;
+  private final MongoCollection<Document> collection;
+
+  public DatabaseManager() {
+    mongoClient = MongoClients.create(URI);
+    MongoDatabase database = mongoClient.getDatabase("FarmFix");
+    this.collection = database.getCollection("TrampleStates");
+    // This should help with faster lookups later
+    this.collection.createIndex(new Document("UUID", 1));
+  }
+
+  // Setting trample to true
+  public void upsertTrample (UUID uuid) {
+    Document filter = new Document("UUID", uuid.toString());
+    Document update = new Document("$set", new Document().append("Trample", true));
+    collection.updateOne(filter, update, new UpdateOptions().upsert(true));
+  }
+
+  // Setting Forced to true
+  public void upsertForced(UUID uuid) {
+    Document filter = new Document("UUID", uuid.toString());
+    Document update = new Document("$set", new Document().append("Forced", true));
+    collection.updateOne(filter, update, new UpdateOptions().upsert(true));
+  }
+
+
+  public void removeTrample(UUID uuid) {
+    Document filter = new Document("UUID", uuid.toString());
+    Document update = new Document("$unset", new Document().append("Trample", ""));
+
+    collection.updateOne(filter,update);
+  }
+
+  public void removeForced(UUID uuid) {
+   Document filter = new Document("UUID", uuid.toString());
+   Document update = new Document("$unset", new Document().append("Forced", ""));
+
+   collection.updateOne(filter,update);
+  }
+
+  public boolean getTrampleState(UUID uuid) {
+    Document filter = new Document("UUID", uuid.toString());
+    Document getDoc = collection.find(filter).first();
+    if (getDoc == null) {
+      return false;
+    }
+    return getDoc.getBoolean("Trample", false);
+  }
+
+  public boolean getForcedState(UUID uuid) {
+    Document filter = new Document("UUID", uuid.toString());
+    Document getDoc = collection.find(filter).first();
+    if (getDoc == null) {
+      return false;
+    }
+    return getDoc.getBoolean("Forced", false);
+  }
+
+  @Override
+  public void close() {
+    mongoClient.close();
+  }
+}
--- src/main/java/dev/cheezboi9/farmfix/managers/TrampleManager.java
+++ src/main/java/dev/cheezboi9/farmfix/managers/TrampleManager.java
@@ -1,83 +1,38 @@
 package dev.cheezboi9.farmfix.managers;
 
-import org.bukkit.Bukkit;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import dev.cheezboi9.farmfix.FarmFix;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.logging.Logger;
 
 public class TrampleManager {
 
   // Appends FarmFix to our logs which is easier to debug later
-  static Logger logger = Logger.getLogger("FarmFix");
-
-  // MongoDB connection string
-  private final static String URI = "mongodb+srv://Cheezboi9:8288523Le.@cheezenvironment.2apl6re.mongodb.net/?retryWrites=true&w=majority&appName=CheezEnvironment";
-
-  // Get the Configuration File
-  private final File CONFIG_FILE = new File(Objects.requireNonNull(Bukkit.getPluginManager().getPlugin("FarmFix")).getDataFolder(),
-      "FarmFix-Trample.yml");
-  private final FileConfiguration CONFIG = YamlConfiguration.loadConfiguration(CONFIG_FILE);
-
-  // Using ConcurrentHashMap instead of a HashMap because it doesn't lock the entire map on access and is threadsafe
-  private final ConcurrentHashMap<UUID, Boolean> playerTrample = new ConcurrentHashMap<>();
-  private final ConcurrentHashMap<UUID, Boolean> forcedTrample = new ConcurrentHashMap<>();
 
   private boolean mobTrample = false;
 
-  public void saveConfig() {
-    try { // Try Catch for most file based operations to prevent crashing
-      CONFIG.save(CONFIG_FILE);
-    } catch (IOException e) {
-      logger.severe("Unable to save FarmFix-Trample.yml : " + e.getMessage());
-    }
 
-  }
-
-  public void loadConfig() {
-    // Early return if no file
-    if (!CONFIG_FILE.exists()) {
-      return;
-    }
-
-    for (String key : CONFIG.getKeys(false)) {
-      try {
-        UUID uuid = UUID.fromString(key);
-        boolean trample = CONFIG.getBoolean(key + ".trample"); // Default false
-        boolean isForcedTrample = CONFIG.getBoolean(key + ".forced"); // Also default false
-        playerTrample.put(uuid, trample);
-        forcedTrample.put(uuid, isForcedTrample);
-
-      } catch (IllegalArgumentException e) {
-        logger.warning("UUID not found in " + CONFIG.getName());
-      }
-    }
-  }
-  // TODO: Do not save default values
   public void toggleTrample(UUID uuid, boolean state) {
-    playerTrample.put(uuid, state);
-    CONFIG.set(uuid + ".trample", state);
-    saveConfig();
-  }
-  // TODO: Do not save default values
-  public void forceTrample(UUID uuid, boolean state) {
-    forcedTrample.put(uuid, state);
-    CONFIG.set(uuid + ".forced", state);
-    saveConfig();
+    if (state) {
+      FarmFix.getDatabaseManager().upsertTrample(uuid);
+    } else {
+      FarmFix.getDatabaseManager().removeTrample(uuid);
+    }
   }
 
+  public void forceTrample(UUID uuid, boolean state) {
+    if (state) {
+      FarmFix.getDatabaseManager().upsertForced(uuid);
+    } else {
+      FarmFix.getDatabaseManager().removeForced(uuid);
+    }
+  }
 
   public boolean isForced(UUID uuid) {
-    return forcedTrample.getOrDefault(uuid, false);
+    return FarmFix.getDatabaseManager().getForcedState(uuid);
   }
 
   public boolean canTrample(UUID uuid) {
-    return playerTrample.getOrDefault(uuid, false);
+    return FarmFix.getDatabaseManager().getTrampleState(uuid);
   }
 
   public void setMobTrample (boolean state) {
